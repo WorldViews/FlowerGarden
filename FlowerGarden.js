17dfcaf1d0dfec84203df4662f3945f39f23e806
donkimber <donkimber@gmail.com>
2020-06-03 08:22:07 -0700

    more cleanup

--- FlowerGarden.js
+++ FlowerGarden.js
@@ -5,107 +5,25 @@
 "use strict";
 
 var ctx;
-var cw, ch, cX, cY;
-var rad = Math.PI / 180;
-var howMany = 4;
-// size of the tangent
+var RAD = Math.PI / 180;
 var t = 1 / 5;
-var petals;
 
+function uniform(lo, hi) {
+  return lo + Math.random() * (hi - lo);
+}
 
 function randomIntFromInterval(mn, mx) {
   return ~~(Math.random() * (mx - mn + 1) + mn);
 }
 
-function getRanPt() {
+function getRanPt(cw, ch) {
   return {
     x: ~~(Math.random() * cw) + 1,
     y: ~~(Math.random() * ch) + 1
   }
 }
 
-function buildRy(R, k, cx, cy, nP, spacing) {
-  var r = R * k;
-  var A = 360 / nP; //nP num petals
-  var petals = [];
-  for (var i = 0; i < nP; i++) {
-    var ry = [];
 
-    ry[ry.length] = {
-      x: cx,
-      y: cy
-    }
-
-    var a1 = i * A + spacing;
-    var x1 = ~~(cx + R * Math.cos(a1 * rad));
-    var y1 = ~~(cy + R * Math.sin(a1 * rad));
-
-    ry[ry.length] = {
-      x: x1,
-      y: y1,
-      a: a1
-    }
-
-    var a2 = i * A + A / 2;
-    var x2 = ~~(cx + r * Math.cos(a2 * rad));
-    var y2 = ~~(cy + r * Math.sin(a2 * rad));
-
-    ry[ry.length] = {
-      x: x2,
-      y: y2,
-      a: a2
-    }
-
-    var a3 = i * A + A - spacing
-    var x3 = ~~(cx + R * Math.cos(a3 * rad));
-    var y3 = ~~(cy + R * Math.sin(a3 * rad));
-
-    ry[ry.length] = {
-      x: x3,
-      y: y3,
-      a: a3
-    }
-
-    ry[ry.length] = {
-      x: cx,
-      y: cy
-    }
-
-    petals[i] = ry;
-
-  }
-  return petals
-}
-
-
-function drawCenter(k, cx, cy, cs) {
-  ctx.beginPath();
-  ctx.fillStyle = cs;
-  ctx.arc(cx, cy, k * 10, 0, 2 * Math.PI)
-  ctx.fill();
-}
-
-function drawCurve(p) {
-
-  var pc = controlPoints(p); // the control points array
-
-  ctx.beginPath();
-  ctx.moveTo(p[0].x, p[0].y);
-  // the first & the last curve are quadratic Bezier
-  // because I'm using push(), pc[i][1] comes before pc[i][0]
-  ctx.quadraticCurveTo(pc[1][1].x, pc[1][1].y, p[1].x, p[1].y);
-
-  if (p.length > 2) {
-    // central curves are cubic Bezier
-    for (var i = 1; i < p.length - 2; i++) {
-      ctx.bezierCurveTo(pc[i][0].x, pc[i][0].y, pc[i + 1][1].x, pc[i + 1][1].y, p[i + 1].x, p[i + 1].y);
-    }
-    // the first & the last curve are quadratic Bezier
-    var n = p.length - 1;
-    ctx.quadraticCurveTo(pc[n - 1][0].x, pc[n - 1][0].y, p[n].x, p[n].y);
-  }
-  ctx.fill();
-}
 
 function controlPoints(p) {
   // given the points array p calculate the control points
@@ -142,34 +60,113 @@ class Flower {
   constructor(garden, opts) {
     this.garden = garden;
     opts = opts || {};
-    var pt = opts.pt || getRanPt();
+    var pt = opts.pt || getRanPt(garden.canvWidth, garden.canvHeight);
     var f = this;
     f.cx = pt.x;
     f.cy = pt.y;
-    f.R = randomIntFromInterval(20, 50);
-    f.Ri = randomIntFromInterval(5, 7) / 10;
-    f.k = randomIntFromInterval(5, 10) / 10;
-    f.ki = randomIntFromInterval(2, 7) / 100;
-    f.K = randomIntFromInterval(5, 16) / 10;
+    f.R = f.R || uniform(10, 15);
+    f.k = uniform(0.5, 1);
+    f.Ri = uniform(.5, .7);
+    f.ki = uniform(.01, 0.04);
+    f.K = uniform(0.7, 1.5);
     f.fs = ~~(Math.random() * garden.colors.length) + 1;
     f.cs = ~~(Math.random() * garden.colors.length) + 1;
-    f.nP = randomIntFromInterval(4, 10);
+    f.numPetals = randomIntFromInterval(4, 10);
     f.spacing = randomIntFromInterval(4, 10);
   }
 
   update() {
     var f = this;
-     if (f.k < f.K) {
+    if (f.k < f.K) {
       f.R += f.Ri;
       f.k += f.ki;
     }
-    var fs = this.garden.colors[f.fs];
-    for (var petal = 0; petal < petals.length; petal++) { //console.log(petals[petal])
-      petals = buildRy(f.R, f.k, f.cx, f.cy, f.nP, f.spacing);
-      ctx.fillStyle = fs;
-      drawCurve(petals[petal]);
+    ctx.fillStyle  = this.garden.colors[f.fs];
+    var petals = this.buildPetals(f.R, f.k, f.cx, f.cy, f.numPetals, f.spacing);
+    for (var petal = 0; petal < petals.length; petal++) {
+      this.drawCurve(petals[petal]);
     }
-    drawCenter(f.k, f.cx, f.cy, f.cs);
+    this.drawCenter(f.k, f.cx, f.cy, f.cs);
+  }
+
+  drawCenter(k, cx, cy, cs) {
+    ctx.beginPath();
+    ctx.fillStyle = cs;
+    ctx.arc(cx, cy, k * 10, 0, 2 * Math.PI)
+    ctx.fill();
+  }
+
+  drawCurve(p) {
+    var pc = controlPoints(p); // the control points array
+    ctx.beginPath();
+    ctx.moveTo(p[0].x, p[0].y);
+    // the first & the last curve are quadratic Bezier
+    // because I'm using push(), pc[i][1] comes before pc[i][0]
+    ctx.quadraticCurveTo(pc[1][1].x, pc[1][1].y, p[1].x, p[1].y);
+
+    if (p.length > 2) {
+      // central curves are cubic Bezier
+      for (var i = 1; i < p.length - 2; i++) {
+        ctx.bezierCurveTo(pc[i][0].x, pc[i][0].y, pc[i + 1][1].x, pc[i + 1][1].y, p[i + 1].x, p[i + 1].y);
+      }
+      // the first & the last curve are quadratic Bezier
+      var n = p.length - 1;
+      ctx.quadraticCurveTo(pc[n - 1][0].x, pc[n - 1][0].y, p[n].x, p[n].y);
+    }
+    ctx.fill();
+  }
+
+  buildPetals(R, k, cx, cy, numPetals, spacing) {
+    var r = R * k;
+    var A = 360 / numPetals;
+    var petals = [];
+    for (var i = 0; i < numPetals; i++) {
+      var ry = [];
+  
+      ry[ry.length] = {
+        x: cx,
+        y: cy
+      }
+  
+      var a1 = i * A + spacing;
+      var x1 = ~~(cx + R * Math.cos(a1 * RAD));
+      var y1 = ~~(cy + R * Math.sin(a1 * RAD));
+  
+      ry[ry.length] = {
+        x: x1,
+        y: y1,
+        a: a1
+      }
+  
+      var a2 = i * A + A / 2;
+      var x2 = ~~(cx + r * Math.cos(a2 * RAD));
+      var y2 = ~~(cy + r * Math.sin(a2 * RAD));
+  
+      ry[ry.length] = {
+        x: x2,
+        y: y2,
+        a: a2
+      }
+  
+      var a3 = i * A + A - spacing
+      var x3 = ~~(cx + R * Math.cos(a3 * RAD));
+      var y3 = ~~(cy + R * Math.sin(a3 * RAD));
+  
+      ry[ry.length] = {
+        x: x3,
+        y: y3,
+        a: a3
+      }
+  
+      ry[ry.length] = {
+        x: cx,
+        y: cy
+      }
+  
+      petals[i] = ry;
+  
+    }
+    return petals
   }
 }
 
@@ -198,14 +195,15 @@ class FlowerGarden {
 
   async loadGarden() {
     var obj = await loadJSON("garden.json");
-    console.log("got garden data: "+JSON.stringify(obj));
+    console.log("got garden data: " + JSON.stringify(obj));
     obj.flowers.forEach(flower => {
-      this.addFlower({x: flower.x, y: flower.y});
+      this.addFlower({ x: flower.x, y: flower.y });
     })
   }
 
   init() {
     var inst = this;
+    this.numInitialFlowers = 8;
     this.requestId = window.requestAnimationFrame(e => inst.update(e));
     window.setInterval(e => {
       if (inst.flowers.length < 200)
@@ -215,10 +213,10 @@ class FlowerGarden {
     $("#" + this.canvasName).mousedown(e => inst.handleClick(e));
     var c = document.getElementById(this.canvasName);
     this.canvas = c;
-    cw = c.width = window.innerWidth;
-    ch = c.height = window.innerHeight;
-    cX = cw / 2,
-      cY = ch / 2;
+    this.canvWidth = c.width = window.innerWidth;
+    this.canvHeight = c.height = window.innerHeight;
+    this.cX = this.canvWidth / 2,
+    this.cY = this.canvHeight / 2;
     ctx = this.canvas.getContext("2d");
     ctx.strokeStyle = "white";
     ctx.shadowBlur = 5;
@@ -231,27 +229,20 @@ class FlowerGarden {
 
   initFlowers() {
     var flowers = this.flowers;
-    for (var hm = 0; hm < howMany; hm++) {
-      this.addFlower();
-    }
-
-    for (var i = 0; i < flowers.length; i++) {
-      var f = flowers[i];
-      petals = buildRy(f.R, f.k, f.cx, f.cy, f.nP, f.spacing);
-      ctx.fillStyle = this.colors[f.fs];
-      for (var i = 0; i < petals.length; i++) {
-        drawCurve(petals[i]);
-      }
+    for (var hm = 0; hm < this.numInitialFlowers; hm++) {
+      this.addFlower().update();
     }
   }
 
   getFlower(pt) {
-    return new Flower(this, {pt});
+    return new Flower(this, { pt });
   }
 
   addFlower(pt) {
     console.log("Adding flower");
-    this.flowers.push(this.getFlower(pt));
+    var f = this.getFlower(pt);
+    this.flowers.push(f);
+    return f;
   }
 
   handleClick(e) {
@@ -264,7 +255,7 @@ class FlowerGarden {
   update() {
     var inst = this;
     var flowers = this.flowers;
-    ctx.clearRect(0, 0, cw, ch);
+    ctx.clearRect(0, 0, this.canvWidth, this.canvHeight);
 
     for (var f = 0; f < flowers.length; f++) {
       var flower = flowers[f];
