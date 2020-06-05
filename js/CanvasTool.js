d38576ed42d41e83fab0c0f15749a461d0cea1c7
donkimber <donkimber@gmail.com>
2020-06-04 20:10:27 -0700

    newer version of CanvasTool

--- js/CanvasTool.js
+++ js/CanvasTool.js
@@ -2,7 +2,9 @@
 
 
 class CanvasTool {
-    constructor(canvasName) {
+    constructor(canvasName, opts) {
+        opts = opts || {};
+        this.timerDelay = opts.timerDelay || 100;
         canvasName = canvasName || "canvas";
         console.log('**** Creating CanvasTool', canvasName);
         this.canvas = document.getElementById(canvasName);
@@ -49,11 +51,17 @@ class CanvasTool {
                 inst.mouseOver(e);
                 return;
             }
+            if (e.which == 1) {
+
+            }
+            inst.handleMouseDrag(e)
+            /*
             var tr = inst.mouseDownTrans;
             var dx = e.clientX - inst.mouseDownPt.x;
             var dy = e.clientY - inst.mouseDownPt.y;
             inst.tx = tr.tx + dx;
             inst.ty = tr.ty + dy;
+            */
             //inst.pan(dx,dy);
             //console.log("move", e);
         });
@@ -64,11 +72,29 @@ class CanvasTool {
         this.canvas.addEventListener("wheel", e => {
             //console.log("mousewheel", e);
             e.preventDefault();
+            if (inst.lockZoom)
+                return;
             if (e.deltaY > 0)
                 inst.zoom(inst.zf);
             else
                 inst.zoom(1 / inst.zf);
         });
+    }
+
+    handleMousePan(e) {
+        var tr = this.mouseDownTrans;
+        var dx = e.clientX - this.mouseDownPt.x;
+        var dy = e.clientY - this.mouseDownPt.y;
+        if (this.panConstraint)
+            [dx,dy] = this.panConstraint(dx,dy);
+        this.tx = tr.tx + dx;
+        this.ty = tr.ty + dy;
+    }
+
+    handleMouseDrag(e) {
+        //console.log("handleMouseDrag", e);
+        if (e.which > 1)
+            this.handleMousePan(e);
     }
 
     mouseMove(e) {
@@ -83,7 +109,7 @@ class CanvasTool {
         //    this.tx, this.ty, this.sx, this.sy);
         $("#canvasStat").html(
             sprintf("pt: %5.1f %5.1f   cpt: %5.1f %5.1f  %s",
-                     pt.x, pt.y, cpt.x, cpt.y, id));
+                pt.x, pt.y, cpt.x, cpt.y, id));
         this.getView();
     }
 
@@ -138,6 +164,12 @@ class CanvasTool {
         this.tx = 0;
         this.ty = 0;
         this.zf = .95;
+        this.aspectRatio = 1;
+        this.lockZoom = false;
+    }
+
+    clear() {
+        this.graphics = {};
     }
 
     /*
@@ -159,26 +191,39 @@ class CanvasTool {
         this.ty += dy;
     }
 
-    setViewRange(xLow, xHigh, yLow, yHigh)
-    {
-        var x = (xLow + xHigh)/2.0;
-        var y = (yLow + yHigh)/2.0;
+    setViewRange(xLow, xHigh, yLow, yHigh) {
+        var x = (xLow + xHigh) / 2.0;
+        var y = (yLow + yHigh) / 2.0;
         var w = xHigh - xLow;
         this.setView(x, y, w);
+    }
+
+    setViewXminYmin(xLow, yLow, w, h) {
+        var view = this.getView();
+        if (w == null) {
+            w = view.width;
+        }
+        if (h == null) {
+            h = view.height;
+        }
+        var x = xLow + w/2.0;
+        var y = yLow + h/2.0;
+         this.setView(x, y, w);
     }
 
     setViewWidth(w) {
         var s = this.canvas.width / (0.0 + w);
         this.sx = s;
-        this.sy = s;
-        this.draw();        
+        if (this.aspectRatio)
+            this.sy = s*this.aspectRatio;
+        this.draw();
     }
 
-    setViewCenter(x,y) {
+    setViewCenter(x, y) {
         var view = this.getView();
         var dx = view.center.x - x;
         var dy = view.center.y - y;
-        this.pan(this.sx*dx,this.sy*dy);
+        this.pan(this.sx * dx, this.sy * dy);
         this.draw();
     }
 
@@ -192,20 +237,19 @@ class CanvasTool {
         if (w != null) {
             this.setViewWidth(w);
         }
-        this.setViewCenter(x,y);
+        this.setViewCenter(x, y);
     }
-    
+
     getView() {
         var cwidth = this.canvas.width;
         var cheight = this.canvas.height;
         var width = cwidth / this.sx;
         var height = cheight / this.sy;
-        var center = this.canvToModel({x: cwidth/2.0, y: cheight/2.0});
-        var view = {center, width, height};
+        var center = this.canvToModel({ x: cwidth / 2.0, y: cheight / 2.0 });
+        var view = { center, width, height };
         //console.log("view", view);
         return view;
     }
-
 
     clearCanvas() {
         var ctx = this.canvas.getContext('2d');
@@ -297,17 +341,25 @@ class CanvasTool {
     }
 
     start() {
-        this.setView(0,0,10);
+        this.setView(0, 0, 10);
         this.tick();
         let inst = this;
-        setInterval(() => inst.tick(), 100);
+        //setInterval(() => inst.tick(), inst.timerDelay);
+        this.requestId = window.requestAnimationFrame(e => inst.handleFrame(e));
+
+    }
+
+    handleFrame(e) {
+        var inst = this;
+        this.tick();
+        this.requestId = window.requestAnimationFrame(e => inst.handleFrame(e));
     }
 
 }
 
 var _graphicNum = 0;
 function getUniqueId() {
-    return "_gr"+_graphicNum++;
+    return "_gr" + _graphicNum++;
 }
 
 function getNumVal(v, def) {
@@ -322,10 +374,13 @@ CanvasTool.Graphic = class {
         this.id = opts.id || getUniqueId(this);
         this.x = opts.x || 0;
         this.y = opts.y || 0;
+        this.width = opts.width;
+        this.height = opts.height;
+        this.textAlign = opts.textAlign || "right";
         this.lineWidth = getNumVal(opts.lineWidth, 0.01);
         this.strokeStyle = '#000';
         this.fillStyle = '#900';
-        this.radius = .1;
+        this.radius = opts.radius || .1;
         this.alpha = 0.333;
         this.clickable = false;
     }
@@ -343,10 +398,31 @@ CanvasTool.Graphic = class {
         ctx.fillStyle = this.fillStyle;
         ctx.beginPath();
         if (this.fillStyle)
-            ctx.fillRect(x-w/2, y-h/2, w, h);
-         ctx.rect(x-w/2, y-h/2, w, h);
-         ctx.stroke();
+            ctx.fillRect(x - w / 2, y - h / 2, w, h);
+        ctx.rect(x - w / 2, y - h / 2, w, h);
+        ctx.stroke();
     }
+
+    drawText(canvas, ctx, x, y, str, font) {
+        font = font || "1px Arial";
+        var s = .2;
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.scale(s,s);
+        ctx.font = font;
+        var metrics = ctx.measureText(str);
+        //let fontHeight = metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent;
+        let actualHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+        //console.log("height:", actualHeight);
+        ctx.translate(0, s*metrics.actualBoundingBoxAscent);
+        ctx.font = font;
+        ctx.textAlign = this.textAlign;
+        ctx.fillStyle = this.textStyle || "black";
+        //ctx.fillText(str, x, y);
+        ctx.fillText(str, 0, 0);
+        ctx.restore();
+    }
+
 
     drawCircle(canvas, ctx, r, x, y) {
         ctx.lineWidth = this.lineWidth;
@@ -382,6 +458,43 @@ CanvasTool.Graphic = class {
     }
 }
 
+CanvasTool.RectGraphic = class extends CanvasTool.Graphic {
+    draw(canvas, ctx) {
+        this.drawRect(canvas, ctx, this.x, this.y, this.width, this.height);
+    }
+
+    contains(pt) {
+        return this.x - this.width / 2 < pt.x && pt.x < this.x + this.width / 2 &&
+            this.y - this.height / 2 < pt.y && pt.y < this.y + this.height / 2;
+    }
+}
+
+CanvasTool.TextGraphic = class extends CanvasTool.Graphic {
+    constructor(opts) {
+        opts.width = opts.width || 1;
+        opts.height = opts.height || 1;
+        opts.textAlign = opts.textAlign || "center";
+        super(opts);
+        this.fillStyle = opts.fillStyle;
+        //this.width = opts.width || 1;
+        //this.height = opts.height || 1;
+        this.text = opts.text || "text";
+        this.textStyle = opts.textStyle || "black";
+    }
+
+    draw(canvas, ctx) {
+        ctx.save();
+        this.drawRect(canvas, ctx, this.x, this.y, this.width, this.height);
+        this.drawText(canvas, ctx, this.x, this.y, this.text);
+        ctx.restore();
+    }
+
+    contains(pt) {
+        return this.x - this.width / 2 < pt.x && pt.x < this.x + this.width / 2 &&
+            this.y - this.height / 2 < pt.y && pt.y < this.y + this.height / 2;
+    }
+}
+
 CanvasTool.IconGraphic = class extends CanvasTool.Graphic {
     constructor(opts) {
         super(opts);
@@ -409,7 +522,7 @@ CanvasTool.CloudGraphic = class extends CanvasTool.Graphic {
         this.r = opts.r || .2;
         this.a0 = .2;
         this.a1 = 0;
-        this.rgb = [200,0,0];
+        this.rgb = [200, 0, 0];
     }
 
     draw(canvas, ctx) {
@@ -418,18 +531,18 @@ CanvasTool.CloudGraphic = class extends CanvasTool.Graphic {
 
     drawCloud(canvas, ctx, pt, rad, rgb, a0, a1) {
         rad = rad;
-        var r,g,b;
-        [r,g,b] = rgb;
+        var r, g, b;
+        [r, g, b] = rgb;
         if (a0 == null)
             a0 = .2;
         if (a1 == null)
             a1 = 0;
-        var color0 = sprintf('rgba(%d,%d,%d,%f)', r,g,b,a0);
-        var color1 = sprintf('rgba(%d,%d,%d,%f)', r,g,b,a1);
+        var color0 = sprintf('rgba(%d,%d,%d,%f)', r, g, b, a0);
+        var color1 = sprintf('rgba(%d,%d,%d,%f)', r, g, b, a1);
         var x = pt[0];
         var y = pt[1];
 
-        var innerRadius = 0.01*rad;
+        var innerRadius = 0.01 * rad;
         var outerRadius = rad;
         var gradient = ctx.createRadialGradient(x, y, innerRadius, x, y, outerRadius);
         gradient.addColorStop(0, color0);
@@ -446,14 +559,14 @@ CanvasTool.GraphGraphic = class extends CanvasTool.Graphic {
         super(opts);
         this.width = opts.width || 10;
         this.height = opts.height || 10;
-        this.rgb = opts.rgb || [200,0,0];
+        this.rgb = opts.rgb || [200, 0, 0];
         this.yvals = [];
         this.maxNumPoints = opts.maxNumPoints || 100;
-        for (var i=0; i<200; i++) {
-            var t = 0.1*i;
+        for (var i = 0; i < 200; i++) {
+            var t = 0.1 * i;
             var w = 3;
             var x = t;
-            var y = Math.sin(w*t);
+            var y = Math.sin(w * t);
             this.addPoint(y);
         }
         //this.lineWidth = .1;
@@ -470,12 +583,12 @@ CanvasTool.GraphGraphic = class extends CanvasTool.Graphic {
         //console.log("GraphGraphic draw", this.points);
         this.drawGraph(canvas, ctx, this.yvals);
     }
-    
+
     drawGraph(canvas, ctx, yvals) {
         ctx.lineWidth = this.lineWidth;
         ctx.strokeStyle = this.strokeStyle;
         ctx.beginPath();
-        var x0 = this.x - this.width/2;
+        var x0 = this.x - this.width / 2;
         var y0 = this.y;
         ctx.moveTo(x0, y0 + yvals[0]);
         for (var i = 1; i < yvals.length; i++) {
@@ -492,7 +605,7 @@ CanvasTool.GraphGraphic = class extends CanvasTool.Graphic {
 CanvasTool.TrailGraphic = class extends CanvasTool.Graphic {
     constructor(opts) {
         super(opts);
-        this.rgb = opts.rgb || [200,0,0];
+        this.rgb = opts.rgb || [200, 0, 0];
         this.points = opts.points || [];
         this.width = getNumVal(opts.width, 10);
         this.height = getNumVal(opts.height, 10);
@@ -520,12 +633,14 @@ CanvasTool.TrailGraphic = class extends CanvasTool.Graphic {
         //console.log("GraphGraphic draw", this.points);
         this.drawTrail(ctx, this.points);
     }
-    
+
     drawTrail(ctx, points) {
+        if (!points || points.length == 0)
+            return;
         ctx.lineWidth = this.lineWidth;
         ctx.strokeStyle = this.strokeStyle;
         ctx.beginPath();
-        var x0 = this.x - this.width/2;
+        var x0 = this.x - this.width / 2;
         var y0 = this.y;
         var p0 = this.points[0];
         ctx.moveTo(x0 + p0[0], y0 + p0[1]);
