04613b4255109e2f615d3a103cc799547ce63991
donkimber <donkimber@gmail.com>
2020-06-14 15:14:24 -0700

    tweaked TaikoGarden stuff

--- python/genTaikoInGarden.py
+++ python/genTaikoInGarden.py
@@ -4,13 +4,19 @@ import json, os
 class Garden:
     def __init__(self):
         self.flowers = []
+        self.pictures = []
     
     def addFlower(self, name, x, y):
-        f = {'type': 'flower', 'x': x, 'y': y}
+        f = {'type': 'flower', 'id': name, 'x': x, 'y': y}
         self.flowers.append(f)
     
+    def addPicture(self, name, url, x, y, width, height):
+        p = {'type': 'Pic', 'name': name,
+             'url': url, 'x': x, 'y': y, 'width': width, 'height': height}
+        self.pictures.append(p)
+
     def getRep(self):
-        return {'flowers': self.flowers}
+        return {'flowers': self.flowers, 'pictures': self.pictures}
     
     def save(self, jsonPath):
         print("saving garden to", jsonPath)
@@ -25,13 +31,16 @@ def genTaikoGarden():
     for name in names:
         if name.lower().endswith('.jpg'):
             images.append(name)
-    wid = 30
+    wd = 100
+    ht = 100
     x = 0
     y = 0
     for name in names:
         print(name)
         g.addFlower(name, x, y)
-        x += wid
+        x += wd
+        url = 'python/taikoin/'+name
+        g.addPicture(name, url, x, y+ht, wd, ht)
 
 
     g.save("taikoInGarden.json")
