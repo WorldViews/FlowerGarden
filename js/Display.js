1756a47dbe01a6a379aa76282e2d8108a1f5faef
donkimber <donkimber@gmail.com>
2020-07-29 14:07:16 -0700

    rm garden2.html and use Display class for floating HUD

--- js/Display.js
+++ js/Display.js
@@ -1,10 +1,18 @@
 "use strict"
 
 class Display {
-    constructor(opts) {
+    constructor(gtool, parentName, opts) {
+        opts = opts || {};
         //super(opts);
         this.name = opts.name;
         this.gtool = gtool;
+        var str = "";
+        str += '<div id="mainScreen">\n';
+        //str += '<iframe id="imageView"></iframe>\n';
+        str += '<img id="imageView" src="">\n';
+        str += '</div>';
+        var div = $(str);
+        div.appendTo($('#'+parentName));
     }
 }
 
