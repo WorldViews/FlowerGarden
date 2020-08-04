e367fb7ab707a5dd29bf2cb32ae7a3d095c3e0d0
donkimber <donkimber@gmail.com>
2020-08-03 20:11:23 -0700

    got PianoBox working enough to see mechanics

--- js/PianoBox.js
+++ js/PianoBox.js
@@ -16,15 +16,47 @@ class PianoBox extends MidiBox {
         opts.instrument = "harpsichord";
         opts.instrument = "acoustic_grand_piano";
         super(opts);
+        this.fillStyle = "salmon";
+        var inst = this;
+        PLAYER.midiPrefix = "/rhythm/midi/";
+        PLAYER.setupTrackInfo();
+        PLAYER.loadInstrument("acoustic_grand_piano");
+        PLAYER.startUpdates();
+        this.startSong();
+        PLAYER.noteObserver = (ch, pitch, v, dur, t) => this.observeNote(ch,pitch, v, dur, t);
     }
 
     onClick() {};
+
+    startSong() {
+        PLAYER.playMelody("Bach/wtc0")
+    }
+
+    observeNote(channel, pitch, vel, t, dur) {
+        var inst = this;
+        //console.log("play note", channel, pitch, vel, dur, t);
+        var i = pitch - 40;
+        let key = this.keys[i];
+        if (!key) {
+            console.log("no note for", pitch);
+            return;
+        }
+        key.fillStyle = key.highlightColor;
+        setTimeout(() => {
+            //console.log("set style", i, prevStyle);
+            key.fillStyle = key.color;
+        }, dur*1000)    ;   
+    }
+
+    highlightKey() {
+
+    }
 
     async addItems() {
         await requirePackage("Taiko");
         console.log("TaikoBox.addItems");
         var opts = { x: this.x, y: this.y, id: "taikobox1" };
-        var numKeys = 40;
+        var numKeys = 50;
         var whiteKeyWidth = 18;
         var blackKeyWidth = 12;
         var spacing = 20;
@@ -36,6 +68,7 @@ class PianoBox extends MidiBox {
         var id;
         var bkeys = [];
         var wkeys = [];
+        this.keys = [];
         for (var i = 0; i < numKeys; i++) {
             var j = i % 12;
             var color = pattern[j];
@@ -45,12 +78,17 @@ class PianoBox extends MidiBox {
                 x += spacing / 2;
             prevColor = color;
             var opts;
+            var key;
             if (color == "white") {
                 id = "wkey" + i;
                 opts = {
                     id, x, y: this.y, width: whiteKeyWidth, height: 100,
                     lineWidth: 1, fillStyle: color, strokeStyle: "black"
                 };
+                key = new PianoKey(opts);
+                key.color = color;
+                key.highlightColor = "pink";
+                wkeys.push(key);
             }
             else {
                 id = "bkey" + i;
@@ -58,14 +96,14 @@ class PianoBox extends MidiBox {
                     id, x, y: this.y -30, width: blackKeyWidth, height: 50,
                     lineWidth: 1, fillStyle: color, strokeStyle: "black"
                 };
+                key = new PianoKey(opts);
+                key.color = color;
+                key.highlightColor = "brown";
+                bkeys.push(key);
             }
-            var key = new PianoKey(opts);
             key.midiId = i+40;
             key.pianoBox = this;
-            if (color=="white")
-                wkeys.push(key);
-            else
-                bkeys.push(key);
+            this.keys.push(key);
             //this.gtool.addGraphic(key);
         }
         var gtool = this.gtool;
