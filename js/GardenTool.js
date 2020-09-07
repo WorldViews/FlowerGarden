04ab800d518b88ae13e611b963e1787b96b4ad26
donkimber <donkimber@gmail.com>
2020-09-06 19:53:14 -0700

    migrate to new firebase project GardenDatabase

--- js/GardenTool.js
+++ js/GardenTool.js
@@ -335,15 +335,16 @@ class GardenTool extends CanvasTool {
     var inst = this;
     if (inst.firebase)
       return;
-    var firebaseConfig = {
-      apiKey: "AIzaSyBqAsqHaBZGT-UsC82ShV3koGWWgu-l8to",
-      authDomain: "fir-helloworld-39759.firebaseapp.com",
-      databaseURL: "https://fir-helloworld-39759.firebaseio.com",
-      projectId: "fir-helloworld-39759",
-      storageBucket: "fir-helloworld-39759.appspot.com",
-      messagingSenderId: "1080893233748",
-      appId: "1:1080893233748:web:1614aab0d167c094322bc1"
-    };
+      var firebaseConfig = {
+        apiKey: "AIzaSyABtA6MxppX03tvzqsyO7Mddc606DsHLT4",
+        authDomain: "gardendatabase-1c073.firebaseapp.com",
+        databaseURL: "https://gardendatabase-1c073.firebaseio.com",
+        projectId: "gardendatabase-1c073",
+        storageBucket: "gardendatabase-1c073.appspot.com",
+        messagingSenderId: "601117522914",
+        appId: "1:601117522914:web:90b28c88b798e45f5fd7bb"
+      };
+
     // Initialize Firebase
     //TODO: move firebase initialization to early place before we
     // go to fetch data.
